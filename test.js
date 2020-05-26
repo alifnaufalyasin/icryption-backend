209f0f70733aeab9e22f1ac8253d067d78c4f920
Abisatria1 <abi.kadek@gmail.com>
2020-05-27 02:57:34 +0800

    file ini digunakan untuk data dummy untuk registrasi CTF

---
+++ test.js
@@ -0,0 +1,23 @@
+const data = 
+[
+    {
+        nama : "Kadek",
+        email : "aa@gmail.com",
+        notelp : "0821631911",
+        status : "Leader"
+    },
+    {
+        nama : "abi",
+        email : "ssss@gmail.com",
+        notelp : "0821631911",
+        status : "Member"
+    },
+    {   
+        nama : "satria",
+        email : "cc@gmail.com",
+        notelp : "0821631911",
+        status : "Member"
+    }
+]
+
+console.log(JSON.stringify(data))
