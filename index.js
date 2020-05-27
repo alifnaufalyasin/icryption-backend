47fbc7d2c31e6b074775bba9d264e9ffa67f6fbc
Abisatria1 <abi.kadek@gmail.com>
2020-05-27 23:01:26 +0800

    menambahkan cors

--- index.js
+++ index.js
@@ -2,12 +2,35 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv').config()
+const cors = require('cors')
 // another file
 const {response} = require('./helpers/wrapper')
 const {deleteFoto} = require('./helpers/validator/validateBody')
 // database and relation
 const db = require('./config/database')
 const relation = require('./config/relation')
+
+// cors
+var allowedOrigins = [
+    "http://127.0.0.1:5501",
+    "http://localhost:3001",
+    "bot-linecoba.herokuapp.com"
+]
+
+app.use(
+    cors({
+        origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg =
+            "The CORS policy for this site does not " +
+            "allow access from the specified Origin.";
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+        }
+    })
+)
 
 // middleware
 app.use(bodyParser.urlencoded({extended : true}))
